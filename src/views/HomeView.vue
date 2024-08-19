57265269199fa75f4b87eb0b60ab1d5e0d65520a
Alicia de Wet <alicia@feeditback.com>
2024-08-19 21:23:30 +0100

    :package: Add tailwind package; set up tailwind; remove sample files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div>Hello</div>
   </main>
 </template>
