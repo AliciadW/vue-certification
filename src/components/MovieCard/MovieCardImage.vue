8ccda388ab735acf5cc4fec19746905921be8e37
Alicia de Wet <alicia@feeditback.com>
2024-08-20 21:15:04 +0100

    :sparkles: add card image component

---
+++ src/components/MovieCard/MovieCardImage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps(['image'])
+</script>
+
+<template>
+  <img class="movie-card-image h-auto w-full rounded-t-lg" :src="image" alt="Movie image" />
+</template>
+
+<style scoped>
+.movie-card-image {
+  max-height: 450px;
+}
+</style>
