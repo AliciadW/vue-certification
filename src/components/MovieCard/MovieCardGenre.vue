f6b04cce84e8c7b09e143db21fc396354aee59d7
Alicia de Wet <alicia@feeditback.com>
2024-08-20 21:14:57 +0100

    :sparkles: add card genre component

---
+++ src/components/MovieCard/MovieCardGenre.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps(['genre'])
+</script>
+
+<template>
+  <div class="movie-card-genre bg-indigo-600 px-1.5 text-white w-fit rounded-full mr-1">
+    {{ genre }}
+  </div>
+</template>
+
+<style scoped>
+.movie-card-genre {
+  font-size: 10px;
+}
+</style>
