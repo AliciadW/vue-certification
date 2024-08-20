6a8c1577b63248b2015d030ebcd45446b3062877
Alicia de Wet <alicia@feeditback.com>
2024-08-20 19:36:02 +0100

    :sparkles: Add Movie Card base view; add movies mock data

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+<script setup>
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <h1 class="text-3xl font-bold underline">Quick test</h1>
-  </header>
-
   <RouterView />
 </template>
-
-<style scoped lang="scss"></style>
---
+++ src/components/MovieCards/MovieCard.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Movie Card</div>
+</template>
+
+<style scoped></style>
