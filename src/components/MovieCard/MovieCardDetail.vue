b78d1aa0d8641f58d1ffbb9ae0bc9fdb19fa4b97
Alicia de Wet <alicia@feeditback.com>
2024-08-20 21:14:50 +0100

    :sparkles: add card detail component

---
+++ src/components/MovieCard/MovieCardDetail.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import MovieCardGenre from '@/components/MovieCard/MovieCardGenre.vue'
+
+const props = defineProps(['item'])
+
+const genres = ref<string[]>(props.item.genres)
+</script>
+
+<template>
+  <div class="flex flex-col bg-white p-3 rounded-b-lg">
+    <h4 class="font-bold text-xl">{{ item.name }}</h4>
+
+    <div class="flex">
+      <MovieCardGenre class="mb-4" v-for="(genre, index) in genres" :key="index" :genre />
+    </div>
+
+    <div class="flex flex-col justify-between">
+      <p class="text-xs h-auto">{{ item.description }}</p>
+      <div class="mt-4">Rating component</div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
