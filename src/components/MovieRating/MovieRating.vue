0ca85f24967f4c3ad702f1827d617f5d8c204c8d
Alicia de Wet <alicia.dewet@gmail.com>
2024-09-09 21:08:42 +0100

    :sparkles: add ratings component

---
+++ src/components/MovieRating/MovieRating.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { StarIcon } from '@heroicons/vue/24/solid'
+
+const props = defineProps(['rating'])
+</script>
+
+<template>
+  <div class="flex">
+    <p class="mr-1">Rating: ({{ rating }})</p>
+    <div class="flex items-center">
+      <span v-for="n in rating" :key="n"> <StarIcon class="size-4 text-yellow-500" /> </span>
+    </div>
+  </div>
+</template>
