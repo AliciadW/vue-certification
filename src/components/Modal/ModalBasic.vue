bcd3e23a2b3e23ea91cb7b3cdc4e71493f399d0a
Alicia de Wet <alicia.dewet@gmail.com>
2024-10-28 19:30:21 +0000

    :sparkles: add basic modal functionality

---
+++ src/components/Modal/ModalBasic.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const props = defineProps(['isOpen'])
+</script>
+
+<template>
+  <template v-if="isOpen">
+    <div class="modal-background" />
+
+    <div class="modal rounded bg-white">
+      <slot />
+    </div>
+  </template>
+</template>
+
+<style scoped>
+.modal-background {
+  background: black;
+  opacity: 65%;
+  height: 100vh;
+  width: 100vw;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.modal {
+  position: fixed;
+  z-index: 999;
+  top: 50%;
+  left: 50%;
+  width: 300px;
+  margin-left: -150px;
+}
+</style>
--- src/views/MovieListView.vue
+++ src/views/MovieListView.vue
@@ -2,6 +2,8 @@
 import { ref } from 'vue'
 
 import MovieCardList from '@/components/MovieCard/MovieCardList.vue'
+import ModalBasic from '@/components/Modal/ModalBasic.vue'
+import AddMovieForm from '@/components/Form/AddMovieForm.vue'
 
 import { items } from '@/assets/movies.json'
 
@@ -16,6 +18,11 @@ interface MovieObject {
 }
 
 const allItems = ref<MovieObject[]>(items)
+const openModal = ref<boolean>(false)
+
+const handleModal = (value: boolean) => {
+  openModal.value = value
+}
 
 const addMovie = (): void => {
   allItems.value.push({
@@ -34,10 +41,13 @@ const addMovie = (): void => {
   <div class="flex flex-col">
     <button
       class="self-end rounded bg-yellow-500 hover:bg-yellow-600 py-1 px-2 mb-8 cursor-pointer font-medium"
-      @click="addMovie"
+      @click="handleModal(true)"
     >
       Add movie
     </button>
     <MovieCardList :items="allItems" />
+    <ModalBasic @open-modal="handleModal(true)" :is-open="openModal">
+      <AddMovieForm @close-modal="handleModal(false)" />
+    </ModalBasic>
   </div>
 </template>
