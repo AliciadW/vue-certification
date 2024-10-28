17e188d974ef4e3da53aa9e5e57f252cd4a4d15c
Alicia de Wet <alicia.dewet@gmail.com>
2024-10-28 20:56:12 +0000

    :poop: add form input binding, get basic functionality done, needs to be improved

--- src/components/Form/AddMovieForm.vue
+++ src/components/Form/AddMovieForm.vue
@@ -1,8 +1,41 @@
 <script setup lang="ts">
-const emit = defineEmits(['close-modal'])
+import { ref } from 'vue'
 
+// TODO: Create types directory and move out
+interface MovieObject {
+  description: string
+  genres: string[]
+  id: number
+  image: string
+  inTheaters: boolean
+  name: string
+  rating: number | null
+}
+
+interface GenreObject {
+  text: string
+  value: string
+}
+
+const emit = defineEmits(['close-modal', 'add-movie'])
+
+const movieData = ref<MovieObject>({
+  description: '',
+  genres: [],
+  id: 0,
+  image: '',
+  inTheaters: true,
+  name: '',
+  rating: 0
+})
 // Random selection of genres
-const genres: string[] = ['Drama', 'Action', 'Comedy', 'Science fiction', 'Adventure']
+const genres = ref<GenreObject[]>([
+  { text: 'Drama', value: 'drama' },
+  { text: 'Action', value: 'action' },
+  { text: 'Comedy', value: 'comedy' },
+  { text: 'Science fiction', value: 'sci-fi' },
+  { text: 'Adventure', value: 'adventure' }
+])
 
 const closeModal = () => {
   // clear form, close modal
@@ -10,10 +43,24 @@ const closeModal = () => {
 }
 
 const addMovie = () => {
+  // TODO: Validation, clean up
+
   // validate form
   // add movie
+  emit('add-movie', movieData.value)
+
   // clear form, close modal
-  console.log('add movie')
+  movieData.value = {
+    description: '',
+    genres: [],
+    id: 0,
+    image: '',
+    inTheaters: true,
+    name: '',
+    rating: 0
+  }
+
+  emit('close-modal')
 }
 </script>
 
@@ -28,6 +75,7 @@ const addMovie = () => {
         name="name"
         required
         size="10"
+        v-model="movieData.name"
       />
     </div>
 
@@ -39,6 +87,7 @@ const addMovie = () => {
         id="description"
         name="description"
         size="10"
+        v-model="movieData.description"
       />
     </div>
 
@@ -50,20 +99,27 @@ const addMovie = () => {
         id="image"
         name="image"
         size="10"
+        v-model="movieData.image"
       />
     </div>
 
     <div class="flex flex-col mb-3">
       <label for="genres">Genre</label>
-      <select class="border rounded-md border-stone-500" name="genres" id="genres" multiple>
-        <option value="">Select a genre</option>
-        <option v-for="(genre, i) in genres" :key="i" value="genre">{{ genre }}</option>
+      <select
+        class="border rounded-md border-stone-500"
+        name="genres"
+        id="genres"
+        multiple
+        v-model="movieData.genres"
+      >
+        <option disabled value="">Please select genres</option>
+        <option v-for="(genre, i) in genres" :key="i" :value="genre.value">{{ genre.text }}</option>
       </select>
     </div>
 
     <div class="flex align-middle mb-3">
       <label class="mr-3" for="inTheatres">In theaters</label>
-      <input type="checkbox" id="inTheatres" name="inTheatres" checked />
+      <input type="checkbox" id="inTheatres" name="inTheatres" v-model="movieData.inTheaters" />
     </div>
 
     <div class="flex justify-between">
